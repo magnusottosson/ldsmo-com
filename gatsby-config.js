cfcf7338d53232a85490251bb08159d01293f38a
Magnus Ottosson <magnus@magnusottosson.se>
2020-11-14 13:30:01 +0100

    Added manifest support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,30 @@
 module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-sass`,
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        background_color: '#000',
+        description: 'LDSMO New Ventures.',
+        display: 'standalone',
+        icon: 'static/black-circle.png',
+        lang: 'en',
+        name: 'LDSMO New Ventures.',
+        short_name: 'LDSMO',
+        start_url: '/',
+        theme_color: '#fff',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        workboxConfig: {
+          globPatterns: ['**/icon-path*'],
+        },
+      },
+    },
     'gatsby-plugin-no-javascript',
     {
       resolve: 'gatsby-plugin-no-javascript-utils',
